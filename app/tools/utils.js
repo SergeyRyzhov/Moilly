5d177468ce65efb65d5f9e0a9d62ad4ec6ce4161
SergeyRyzhov <serega.rf@gmail.com>
2015-11-24 16:53:18 +0300

    Cleanup

--- app/tools/utils.js
+++ app/tools/utils.js
@@ -1,17 +1,12 @@
 var _ = require('underscore');
 
 function merge(target, source) {
-	//console.log('t', target);	
-	//console.log('s', source);
-	
 	_.defaults(target, source);
 	for (var prop in target) {
-		if (target.hasOwnProperty(prop) && typeof(source[prop]) ==='object') {
+		if (target.hasOwnProperty(prop) && typeof (source[prop]) === 'object') {
 			merge(target[prop], source[prop]);
 		}
-	}	
-	
-	console.log('t', target);	
+	}
 	return target;
 }
 
